ebc84db2280435dc15e4df98fc618732b71017a5
Till Klocke <till.klocke@grandcentrix.net>
2023-05-25 11:44:49 +0200

    the model name can now be set, but this is optional

--- vc_ai.py
+++ vc_ai.py
@@ -3,8 +3,8 @@ from langchain.embeddings import OpenAIEmbeddings
 from langchain.llms.base import LLM
 from langchain.embeddings.base import Embeddings
 
-def build_openai_llm(model_name: str = "ada-search-document") -> LLM:
-    llm = OpenAI()
+def build_openai_llm(model_name: str = None) -> LLM:
+    llm = OpenAI(model_name=model_name)
     return llm
 
 def build_openai_embedding()->Embeddings:
